d34345542277c5c40ce9818afaa2f74d6092cadd
bambi teague <teeegsa@gmail.com>
2020-10-23 18:14:30 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown(count = 10)
+    while count > 0
+        puts "#{count} SECOND(S)!"
+          count -= 1
+        
+    end 
+    "HAPPY NEW YEAR!"   
 end
+
+def countdown_with_sleep(time = countdown)
+    while time > 0
+        sleep 1
+        time -= 1
+    end 
+    binding.pry   
+end
+
+
+#countdown outputs '<n> SECOND(S)!' string to STDOUT for each count
